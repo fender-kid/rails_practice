bc86ab425d7dfe26b798fd466f447a28f122dc90
James Sides <jtt.sides@gmail.com>
2024-01-29 16:34:52 -0600

    more practice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_220255) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_222911) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
